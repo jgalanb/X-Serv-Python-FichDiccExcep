838ed6ed0e50213f271308f66b567d7dbc14eec3
Jesús <jgalanb>
2016-02-07 19:18:39 +0100

    Entrega ejercicio 13.5 por Jesus Galan Barba

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# hola, hola
+# Jesús Galán Barba
+# Ing. en Sitemas de Telecomunicaciones
 
-fd = open('/etc/passwd', 'r')
+print "\nEjercicio 13.5: Ficheros, diccionarios y excepciones\n"
 
+fd = open("/etc/passwd","r")
 lineas = fd.readlines()
-fd.close()
+usuarios = {}
 
 for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+	trozos_linea = linea.split(':')
+	usuario = trozos_linea[0]
+	shell = trozos_linea[-1]
+	usuarios[usuario] = shell
+
+fd.close()
+
+try:
+	claves = ['root','imaginario']
+	for clave in claves:
+		shell_diccionario = usuarios[clave]
+		print "Usuario:", clave, "=> Shell:", shell_diccionario[0:-1]
+
+except KeyError:
+	print "ERROR: Usuario '", clave, "' no encontrado en el diccionario"
